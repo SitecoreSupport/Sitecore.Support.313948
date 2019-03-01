6504db31a53e3f9b990144655f85435a0985639b
dtat <dtat@sitecore.net>
2019-03-01 12:23:55 +0200

    Enabled mappings.

---
+++ src/Sitecore.Support.313948/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.313948")]
+[assembly: AssemblyProduct("Sitecore.Support.313948")]
+[assembly: ComVisible(false)]
